5152ffcf6c69d70a4eda64b7794d6f4b15fd73ce
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-03-06 12:45:06 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Home from './components/Home.vue'
+import Projects from './components/Projects.vue'
+import Contacts from './components/Contacts.vue'
+</script>
+
+<template>
+  <Home />
+  <Projects />
+  <Contacts />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
