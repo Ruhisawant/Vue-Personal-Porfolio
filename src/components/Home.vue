a634ef9ddfc74de5b5746b3c852a58bd67791d4c
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-03-06 20:02:38 -0500

    Added toggle buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,28 @@
 import { ref } from 'vue'
 
 const welcome = ref(true);
+const counter = ref(0);
+const showMessage = ref(false);
+
+const increment = () => {
+  counter.value++;
+}
 </script>
 
 <template>
   <section class="home">
     <div>Home</div>
+    
     <h1 v-if="welcome">Welcome!</h1>
+    <h1 v-else>Hello</h1>
+
+    <button @click="welcome = !welcome">Toggle</button>
+
+    <button @click="increment">Visitor Counter</button>
+
+    <button @click="showMessage = !showMessage">Toggle Message</button>
+    <p v-show="showMessage">Counter: {{ counter }}</p>
+    
   </section>
     
 </template>
